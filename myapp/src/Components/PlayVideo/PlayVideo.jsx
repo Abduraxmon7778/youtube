5bc2832df3fec4a8674b4d36e08eb0d4d9f6c2e5
[Abduraxmon Avazov] <abduraxmonavazov8@gmail.com>
2025-09-12 09:58:24 +0500

    first commit

---
+++ myapp/src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = () => {
+  return (
+    <div className='play-video'>
+      <video src={video1} controls autoPlay muted ></video>
+      <h3>Best Youtube Channel To Learn Web Developmer</h3>
+      <div className="play-video-info">
+        <p>1525 Views &bull; 2days ago</p>
+      </div>
+      <div>
+        <span><img src={like} alt="" /> 125</span>
+        <span><img src={like} alt="" /> 125</span>
+        <span><img src={like} alt="" /> 125</span>
+        <span><img src={like} alt="" /> 125</span>
+      </div>
+    </div>
+  )
+}
+
+export default PlayVideo
