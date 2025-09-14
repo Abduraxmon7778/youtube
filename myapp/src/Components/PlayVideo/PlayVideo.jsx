4f97a519f1594d902e07dbbe7c72fb432604c8c6
[Abduraxmon Avazov] <abduraxmonavazov8@gmail.com>
2025-09-14 20:14:55 +0500

    Сохранил изменения

--- myapp/src/Components/PlayVideo/PlayVideo.jsx
+++ myapp/src/Components/PlayVideo/PlayVideo.jsx
@@ -6,6 +6,7 @@ import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import save from '../../assets/save.png'
 
 const PlayVideo = () => {
   return (
@@ -14,12 +15,77 @@ const PlayVideo = () => {
       <h3>Best Youtube Channel To Learn Web Developmer</h3>
       <div className="play-video-info">
         <p>1525 Views &bull; 2days ago</p>
-      </div>
+      
       <div>
         <span><img src={like} alt="" /> 125</span>
-        <span><img src={like} alt="" /> 125</span>
-        <span><img src={like} alt="" /> 125</span>
-        <span><img src={like} alt="" /> 125</span>
+        <span><img src={dislike} alt="" /> 2</span>
+        <span><img src={share} alt="" /> Share</span>
+        <span><img src={save} alt="" /> Save</span>
+      </div>
+      </div>
+      <hr />
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>GreatStack</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>Channel that makes learning Easy</p>
+        <p>Subscribe GreatStack to Watch More Tutorials on web development</p>
+        <hr />
+        <h4>130 Comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque placeat veritatis aperiam distinctio fugiat, hic sunt repellendus minus earum incidunt, aut architecto. Ex omnis veniam eveniet provident nihil, dolores placeat?</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque placeat veritatis aperiam distinctio fugiat, hic sunt repellendus minus earum incidunt, aut architecto. Ex omnis veniam eveniet provident nihil, dolores placeat?</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque placeat veritatis aperiam distinctio fugiat, hic sunt repellendus minus earum incidunt, aut architecto. Ex omnis veniam eveniet provident nihil, dolores placeat?</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque placeat veritatis aperiam distinctio fugiat, hic sunt repellendus minus earum incidunt, aut architecto. Ex omnis veniam eveniet provident nihil, dolores placeat?</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
